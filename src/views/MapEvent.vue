765ed6cdb26d9a3a8e3cbf6b103c670c012ca590
jack <jackzhoumine@gmail.com>
2024-02-25 02:17:14 +0800

    feat: 事件

--- src/views/MapEvent.vue
+++ src/views/MapEvent.vue
@@ -3,7 +3,7 @@ import { ref, watch } from "vue";
 import { useInitMap, useClickMap, useZoomMap } from "@/hooks";
 
 const mapContainer = ref(null);
-const { map } = useInitMap(mapContainer, {}, ['AMap.ToolBar', 'AMap.Scale', 'AMap.HawkEye', 'AMap.MapType', 'AMap.ControlBar'])
+const { map } = useInitMap(mapContainer, {}, ['AMap.ToolBar', 'AMap.Scale', 'AMap.MapType', 'AMap.ControlBar'])
 const { position } = useClickMap(map)
 const { zoom } = useZoomMap(map)
 </script>
@@ -12,7 +12,7 @@ const { zoom } = useZoomMap(map)
   <div class='w-full h-full' ref="mapContainer"></div>
   <div class="info">
     <div class="lng">lng : {{ position.lng }} </div>
-    <div>lat : {{ position.lat }}</div>
+    <div class="lng">lat : {{ position.lat }}</div>
     <div>zoom : {{ zoom }}</div>
   </div>
 </template>
@@ -20,7 +20,7 @@ const { zoom } = useZoomMap(map)
 <style scoped lang="scss">
 .info {
   position: absolute;
-  left: calc(50% - 50px);
+  left: calc(50% - 150px);
   bottom: 10px;
   background-color: aliceblue;
   display: flex;
